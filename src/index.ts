dc95f78aee711c56e1dbaa04350b0c45e72e8ac4
qiansc <van3008@163.com>
2018-10-29 23:13:14 +0800

    hello rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function hello () {
+/*
+ * @Author: qiansc
+ * @Date: 2018-10-29 23:12:50
+ * @Last Modified by:   qiansc
+ * @Last Modified time: 2018-10-29 23:12:50
+ */
+export function hello() {
     console.log("hello");
-};
+}
